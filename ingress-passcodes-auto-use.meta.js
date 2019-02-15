0b8f06d05946e68e0f4b247cf1c90d475979a111
yesnosurely <33597847+yesnosurely@users.noreply.github.com>
2019-02-15 13:55:33 +0200

    Update and rename ingress-passcodes-auto-use-iitc.meta.js to ingress-passcodes-auto-use.meta.js

--- ingress-passcodes-auto-use.meta.js
+++ ingress-passcodes-auto-use.meta.js
@@ -2,9 +2,9 @@
 // @name          Ingress passcodes auto use
 // @author        @yesnosurely
 // @namespace     https://github.com/yesnosurely/ingress-passcodes-auto-use/
-// @version       1
-// @updateURL     https://raw.githubusercontent.com/yesnosurely/ingress-passcodes-auto-use/master/ingress-passcodes-auto-use-iitc.meta.js
-// @downloadURL   https://raw.githubusercontent.com/yesnosurely/ingress-passcodes-auto-use/master/ingress-passcodes-auto-use-iitc.user.js
+// @version       2
+// @updateURL     https://raw.githubusercontent.com/yesnosurely/ingress-passcodes-auto-use/master/ingress-passcodes-auto-use.meta.js
+// @downloadURL   https://raw.githubusercontent.com/yesnosurely/ingress-passcodes-auto-use/master/ingress-passcodes-auto-use.user.js
 // @require       http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js
 // @match         http://intel.ingress.com/*
 // @match         https://intel.ingress.com/*
